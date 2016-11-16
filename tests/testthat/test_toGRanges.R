fd795578c72d5afa69095d9833bfb0878b72dd55
j.ou <j.ou@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-11-16 15:38:59 +0000

    trim seqnames when using toGRanges
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_4/madman/Rpacks/ChIPpeakAnno@124209 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_toGRanges.R
+++ tests/testthat/test_toGRanges.R
@@ -11,12 +11,20 @@ test_that("toGRanges works not correct", {
         expect_equal(start(rd), start(gr))
         expect_equal(end(rd), end(gr))
     }
-    
+  
     ##BED
     bed <- data.frame(cbind(space = c("1", "2"), start=c("100", "1000"),
                 end=c("200", "1100"), name=c("peak1", "peak2")))
     gr <- toGRanges(bed, format="BED")
     expect_equal(start(gr), c(101, 1001))
+    
+    ##trim space
+    bed$space <- paste(" ", 1:2)
+    gr <- toGRanges(bed, format="BED")
+    expect_equal(seqlevels(gr), c("1", "2"))
+    bed$space <- paste(1:2, " ")
+    gr <- toGRanges(bed, format="BED")
+    expect_equal(seqlevels(gr), c("1", "2"))
     
     ##data.frame
     gr <- toGRanges(bed)
